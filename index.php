8eb98375ded79d9e052cc4b45bcb1a86a931f8ef
Vane <vsalas@knights.ucf.edu>
2022-04-30 17:43:55 -0400

    portfolio2

--- index.php
+++ index.php
@@ -8,18 +8,21 @@
   <body>
 <?php
 
-        echo("Welcome, choose to clock in or out");
+        echo("Welcome to the employee timecard system!");
+        echo ("</br>");  
         echo("<form action='signup.php' method='POST'>");
             
         echo("<label>First Name</label>");
-        echo("<input type='text' placeholder='Firstname' name='firstname'>");
+        echo("<input type='text' placeholder='First Name' name='firstname'>");
 
-        echo "</br>";  
+        echo ("</br>");  
         
         echo("<label>Last Name</label>");
-        echo("<input type='text' placeholder='Lastname' name='lastname'>");
+        echo("<input type='text' placeholder='Last Name' name='lastname'>");
 
-        echo "</br>";  
+        echo ("</br>");  
+        echo ("</br>");  
+
         echo("<button type='submit'>Clock In</button>");
         echo("<button type='submit'>Clock Out</button>");
 
